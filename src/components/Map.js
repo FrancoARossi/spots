c4eeeeb6e6d12551201633ce57df22f155784035
Franco Rossi <rossifrancoa@gmail.com>
2020-11-11 22:59:54 -0300

    Added user marker on Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { RiWalkLine } from "react-icons/ri";
 
 function Map() {
   const [viewport, setViewport] = useState({
@@ -10,24 +11,79 @@ function Map() {
     zoom: 15,
   });
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      //TODO: Add contemplation for a denied access to position (error)
-      setViewport({
-        ...viewport,
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude,
-      });
-    });
+  const [userPosition, setUserPosition] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
   });
+
+  const positionOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+  };
+
+  const onViewportChange = (newViewport) => {
+    setViewport({ ...newViewport });
+  };
+
+  const successPosition = (pos) => {
+    setViewport({
+      ...viewport,
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude,
+    });
+
+    // TODO: eliminar setUserPosition cuando se solucione el problema con setInterval
+    setUserPosition({
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude,
+    });
+  };
+
+  const errorPosition = (error) => {
+    console.warn("ERROR(" + error.code + "): " + error.message);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      successPosition,
+      errorPosition,
+      positionOptions
+    );
+    // eslint-disable-next-line
+  }, []);
+
+  // TODO: solucionar el setInterval para actualizar posicion por que laggea y termina crasheando
+  /*
+  const successUserPosition = (pos) => {
+    setUserPosition({
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude,
+    });
+  };
+  
+  setInterval(() => {
+    navigator.geolocation.getCurrentPosition(
+      successUserPosition,
+      errorPosition,
+      positionOptions
+    );
+  }, 5000);
+  */
 
   return (
     <ReactMapGL
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       mapStyle="mapbox://styles/francoarossi/ckhe0w2rt08ff19nyff5iii23"
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    />
+      onViewportChange={onViewportChange}
+    >
+      <Marker
+        latitude={userPosition.latitude}
+        longitude={userPosition.longitude}
+      >
+        <RiWalkLine size={32} />
+      </Marker>
+    </ReactMapGL>
   );
 }
 
