b73bd387c63a9b46d1e43e8dd7dd06bfefde81c6
Ivan <im.moralesivan@gmail.com>
2020-11-19 21:35:27 -0300

    Icons and favicon updated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { FaListUl } from "react-icons/fa";
 
 export default function Navigation(){
     return (
 
-        <div style={{ position: "fixed", background: "#fff", bottom: "0", width: "100%", display: "flex", justifyContent: "space-around", padding: "1em", zIndex: "10"}}>
-            <Link to="/"><button className="btn btn-primary">Mapa</button></Link>
-            <Link to="/spotList"><button className="btn btn-primary">Lista</button></Link>
+        <div style={{ position: "fixed", background: "#fff", bottom: "0", width: "100%", display: "flex", justifyContent: "space-evenly", padding: "1em", zIndex: "10"}}>
+            <Link to="/"><button className="btn btn-lg btn-primary"><FaMapMarkedAlt /></button></Link>
+            <Link to="/spotList"><button className="btn btn-lg btn-primary"><FaListUl /></button></Link>
         </div>
     );
 }
