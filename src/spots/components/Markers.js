e9764003853fa5a278b07147f1c9681d05ac730e
Franco Rossi <francorossi@sirius.com.ar>
2020-12-13 19:17:54 -0300

    Minor Fixes

--- src/spots/components/Markers.js
+++ src/spots/components/Markers.js
@@ -26,7 +26,6 @@ const SpotsMarkers = ({ spotsList, status, userPosition, getSpotsRequest }) => {
             }}
             className="btn btn-sm"
           >
-            {/*TODO: Solucionar warnings del svg*/}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="30"
@@ -34,9 +33,9 @@ const SpotsMarkers = ({ spotsList, status, userPosition, getSpotsRequest }) => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="#8300ff"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z" />
               <circle cx="12" cy="10" r="3" />
